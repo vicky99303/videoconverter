55c02e0551dcdc7003b7ce1874680796907c4dbd
abdul_waqar <abdulwaqar4@gmail.com>
2021-08-27 13:36:26 +0500

    push code on cloud

---
+++ app/Http/Controllers/VideoFiles.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\VideoFileModel;
+use Illuminate\Http\Request;
+
+class VideoFiles extends Controller
+{
+    public function createForm()
+    {
+        return view('fileupload');
+    }
+
+    public function fileUpload(Request $req)
+    {
+        $req->validate([
+            'file' => 'required|mimes:csv,txt,xlx,xls,pdf|max:2048'
+        ]);
+
+        $fileModel = new VideoFileModel;
+
+        if ($req->file()) {
+            $fileName = time() . '_' . $req->file->getClientOriginalName();
+            $filePath = $req->file('file')->storeAs('uploads', $fileName, 'public');
+
+            $fileModel->name = time() . '_' . $req->file->getClientOriginalName();
+            $fileModel->file_path = '/storage/' . $filePath;
+            $fileModel->save();
+
+            return back()
+                ->with('success', 'File has been uploaded.')
+                ->with('file', $fileName);
+        }
+    }
+}
---
+++ app/Models/VideoFileModel.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class VideoFileModel extends Model
+{
+    use HasFactory;
+
+    protected $table = 'files';
+    protected $fillable = [
+        'name',
+        'file_path'
+    ];
+}
